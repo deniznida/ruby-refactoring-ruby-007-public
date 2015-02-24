97e8c723ee92f221d5a5548e297e9ace393e0768
deniznida <denizunat@hotmail.com>
2015-02-23 21:44:15 -0500

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
-  attr_accessor :leash, :plastic_bag, :walking, :vet
-
+  attr_accessor :walking, :vet, :owner, :leash, :plastic_bag, :vet_checkup
+  
   def initialize(name, breed, owner_name)
     @name = name
     @breed = breed
-    @owner = owner_name
-    @leash = false
-    @plastic_bag = false
     @walking = false
     @vet_checkup = false
+    @leash = false
+    @plastic_bag = false
+    @owner = Owner.new(owner_name, self)
   end
 
   def name
@@ -23,31 +23,11 @@ class Dog
     @breed
   end
 
-  def owner
-    @owner
+  def walking?
+    self.owner.walking
   end
 
-  def walking?(owner)
-    owner.dog.walking
-  end
-
-  def vet_checkup?(owner)
-    owner.dog.vet_checkup
-  end
-
-  # Refactor the following methods as methods in the Owner class!
-
-  def walk(owner)
-    # REFACTOR!
-    owner.dog.leash = true
-    owner.dog.plastic_bag = true
-    owner.dog.walking = true
-  end
-
-  def vet_checkup(owner)
-    # REFACTOR!
-    owner.dog.leash = true
-    owner.dog.plastic_bag = true
-    owner.dog.vet_checkup = true
+  def vet_checkup?(name)
+    name.vet_checkup
   end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,31 @@
 class Owner
-  # code your solution here!
+  attr_accessor :name, :dog, :leash, :plastic_bag
+
+  def initialize(name, dog)
+    @name = name
+    @dog = dog
+    @leash = false
+    @plastic_bag = false
+  end
+
+  def prepare_items
+    @leash = true
+    @plastic_bag = true
+    self.dog.leash = true
+    self.dog.plastic_bag = true
+  end
+
+  def walk
+    prepare_items
+    self.dog.walking = true
+  end
+
+  def vet_checkup
+    prepare_items
+    self.dog.vet_checkup = true
+  end
+
+  def walking?
+    !!walk
+  end
 end
